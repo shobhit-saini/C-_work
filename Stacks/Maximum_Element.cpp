26982b60643dc7a613f9237fbabe9df310d33ebf
SHOBHIT <shobhitsaini95@gmail.com>
2019-08-19 10:05:13 +0530

    Create Maximum_Element.cpp

---
+++ Stacks/Maximum_Element.cpp
@@ -0,0 +1,15 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <stack>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int N, i = 0, ele = 0, max_ele = 0, x = 0, options = 0;
+    stack<int> st;  
+    stack<int> max_st;
+    cin>> N;
