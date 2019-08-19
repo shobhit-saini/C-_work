8db05bdf70548beb1d3c92efb1d5b80d1f8f8a61
SHOBHIT <shobhitsaini95@gmail.com>
2019-08-19 10:07:05 +0530

    Update Maximum_Element.cpp

--- Stacks/Maximum_Element.cpp
+++ Stacks/Maximum_Element.cpp
@@ -8,7 +8,6 @@ using namespace std;
 
 
 int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
     int N, i = 0, ele = 0, max_ele = 0, x = 0, options = 0;
     stack<int> st;  
     stack<int> max_st;
