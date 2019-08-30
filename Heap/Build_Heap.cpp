d1360bf590b8cca6bafe0c1885c19008dc7b95f3
SHOBHIT <shobhitsaini95@gmail.com>
2019-08-31 00:21:20 +0530

    Create Build_Heap.cpp

---
+++ Heap/Build_Heap.cpp
@@ -0,0 +1,19 @@
+int Build_heap(int *ar, int low, int high)
+{
+	int  temp = 0, i = 0;
+	for(i = floor(high/2) - 1; i >= 0; i--)
+	{
+		if(ar[i] > ar[2*i + 1] || ar[i] > ar[2*i + 2])
+		{
+			if(ar[2*i + 1] < ar[2*i + 2])
+			{
+				Swap(&ar[i], &ar[2*i + 1]);
+			}
+			else
+			{
+				Swap(&ar[i], &ar[2*i + 2]);
+			}
+		}
+	}
+return 0;
+}
