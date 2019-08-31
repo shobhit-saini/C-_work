bdaa0989cb3118eca9242654ffe77b52942844dd
SHOBHIT <shobhitsaini95@gmail.com>
2019-08-31 22:19:18 +0530

    Update Build_Heap.cpp

--- Heap/Build_Heap.cpp
+++ Heap/Build_Heap.cpp
@@ -1,19 +1,34 @@
-int Build_heap(int *ar, int high)
+int Build_heap(struct Huffman *ar, int high)
 {
-	int  temp = 0, i = 0;
-	for(i = floor(high/2) - 1; i >= 0; i--)
+	int  temp = 0, i = 0, j = 0;
+	if(high == 2)
 	{
-		if(ar[i] > ar[2*i + 1] || ar[i] > ar[2*i + 2])
+		if(ar[i].frequency > ar[2*i + 1].frequency)
 		{
-			if(ar[2*i + 1] < ar[2*i + 2])
+			Swap(&ar[i], &ar[2*i + 1]);
+		}
+	}
+	else
+	{
+	
+		for(i = floor(high/2) - 1; i >= 0; i--)
+		{
+			j = i;
+			while(j <= floor(high/2) - 1)
 			{
-				Swap(&ar[i], &ar[2*i + 1]);
-			}
-			else
-			{
-				Swap(&ar[i], &ar[2*i + 2]);
+				if(ar[j].frequency > ar[2*j + 1].frequency || ar[j].frequency > ar[2*j + 2].frequency)
+				{
+					if(ar[2*j + 1].frequency < ar[2*j + 2].frequency)
+					{
+						Swap(&ar[j], &ar[2*j + 1]);
+						j = 2*j + 1;
+					}
+					else
+					{
+						Swap(&ar[j], &ar[2*j + 2]);
+						j = 2*j + 2;
+					}
+				}
 			}
 		}
 	}
-return 0;
-}
