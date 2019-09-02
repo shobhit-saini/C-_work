e4c9340b64b8c1c012b39d9d2396e5cba4ba9987
SHOBHIT <shobhitsaini95@gmail.com>
2019-09-02 19:58:50 +0530

    Update Build_Heap.cpp

--- Heap/Build_Heap.cpp
+++ Heap/Build_Heap.cpp
@@ -1,3 +1,23 @@
+#include<iostream>
+#include<Math.h>
+using namespace std;
+
+struct Huffman
+{
+	char data;
+	int frequency;
+	struct Huffman *left;
+	struct Huffman *right;
+};
+
+int Swap(struct Huffman *a, struct Huffman *b)
+{
+	struct Huffman temp;
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
 int Build_heap(struct Huffman *ar, int high)
 {
 	int  temp = 0, i = 0, j = 0;
@@ -16,19 +36,65 @@ int Build_heap(struct Huffman *ar, int high)
 			j = i;
 			while(j <= floor(high/2) - 1)
 			{
-				if(ar[j].frequency > ar[2*j + 1].frequency || ar[j].frequency > ar[2*j + 2].frequency)
+				if((2*j +2 < high))
 				{
-					if(ar[2*j + 1].frequency < ar[2*j + 2].frequency)
+					if((ar[j].frequency > ar[2*j + 1].frequency) || (ar[j].frequency > ar[2*j + 2].frequency))
 					{
-						Swap(&ar[j], &ar[2*j + 1]);
-						j = 2*j + 1;
+						if((ar[2*j + 1].frequency < ar[2*j + 2].frequency))
+						{
+							Swap(&ar[j], &ar[2*j + 1]);
+							j = 2*j + 1;
+						}
+						else
+						{
+							Swap(&ar[j], &ar[2*j + 2]);
+							j = 2*j + 2;
+						}
 					}
+					
 					else
 					{
-						Swap(&ar[j], &ar[2*j + 2]);
-						j = 2*j + 2;
+						break;
 					}
+						
+				}
+				else if(ar[j].frequency > ar[2*j + 1].frequency)
+				{
+					Swap(&ar[j], &ar[2*j + 1]);
+						j = 2*j + 1;
+				}
+				else
+				{
+					break;
 				}
 			}
+		}
+	}
+return 0;
+}
+	int main()
+	{
+		int i = 0, N = 0;
+		cout << "Enter the number of characters:\n";
+		cin >> N;
+		struct Huffman ar[2*N];
+		cout << "Enter the character and it's frequency:";
+		
+		for(i = 0; i < N; i++)
+		{
+			cin >> ar[i].data;
+			cin >> ar[i].frequency;
+			ar[i].left = NULL;
+			ar[i].right = NULL;
+		}	
+		
+		Build_heap(ar, N);
+		
+		for(i = 0; i < N; i++)
+		{
+			cout <<  ar[i].data << "\t";
+			cout << ar[i].frequency << "\t";
+			cout << ar[i].left << "\t";
+			cout << ar[i].right << "\n";
 		}
 	}
